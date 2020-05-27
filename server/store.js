b7faf515d841cb8b24c8b762c22cd268dc48e284
Bartosz Chadzyjanidis <bartoszchadzyjanidis@gmail.com>
2020-05-27 20:04:19 +0200

    Dodano funkcję dodawania zadań

--- server/store.js
+++ server/store.js
@@ -1,7 +1,7 @@
 const storage = require('azure-storage')
 const service = storage.createTableService('bartoszchadzyjanidis','dLHvFAmYrIlthHs52V4WJ2sBZ9IB5hET1MRsB38sC9dq5nrmGu0w2PdnUU++kGBTm5pU7J43wQSGPttTwc7k/w==')
 const table = 'tasks'
-
+const uuid = require('uuid')
 const init = async () => (
   new Promise((resolve, reject) => {
     service.createTableIfNotExists(table, (error, result, response) => {
@@ -9,7 +9,22 @@ const init = async () => (
     })
   })
 )
+const addTask = async ({ title }) => (
+  new Promise((resolve, reject) => {
+    const gen = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: gen.String('task'),
+      RowKey: gen.String(uuid.v4()),
+      title
+    }
+
+    service.insertEntity(table, task, (error) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
 
 module.exports = {
-  init
+  init,
+  addTask
 }
